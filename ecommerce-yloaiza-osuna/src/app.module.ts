c8639358bf3fb5f1e98711ef4b48db9b892701aa
yesidloaiza <losunayes@gmail.com>
2025-10-08 00:02:18 -0500

    nestjs fundamentalsI

---
+++ ecommerce-yloaiza-osuna/src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [UsersModule, ProductsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
